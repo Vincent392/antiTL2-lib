ab711e2ffeb297e0da96cdcb98c4375a7c0c5fcc
Vincent392 <90470156+Vincent392@users.noreply.github.com>
2023-10-11 22:09:37 +0100

    update it

--- com/Vincent/TL2/detect/TL2detect.java
+++ com/Vincent/TL2/detect/TL2detect.java
@@ -1,14 +1,17 @@
-package com.Vincent.TL2.detect; //feel free to add this to any mod
+package com.Vincent.TL2.detect;
 
 import java.awt.Component;
 import javax.swing.JOptionPane;
-import java.io.File;
 
 public class TL2detect
 {
+ // author: Vincent
+ // made using: Eclipse IDE + Github Mobile
+ //feel free to add this to any mod
     public static void main(final String[] args) {
-        final File f = new File("C:\\Users\\" + System.getProperty("user.home") + "\\AppData\\Roaming\\.minecraft\\TLauncher.exe");
-        if (f.exists()) {
+        //final File f = new File("C:\\Users\\" + System.getProperty("user.home") + "\\AppData\\Roaming\\.minecraft\\TLauncher.exe"); old code
+        //if (f.exists()) {
+          if (args == "-tl2proxy")) {
             System.out.println("TL2 Detected!");
             System.out.println("BOOT ERR! GIVING WARNING!");
             JOptionPane.showMessageDialog(null, "A boot Error happened!", "Error!", 0);
